3e48aad332dcb26cc2f61c1e007dcdde777e90d9
AnandaRehan <anandarehan071@gmail.com>
2026-08-31 09:52:11 +0700

    fix: 19

--- app/src/main/java/com/ehan/app2/MainActivity.kt
+++ app/src/main/java/com/ehan/app2/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 //import androidx.compose.runtime.*
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.LaunchedEffect
