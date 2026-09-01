d807a0caa8d17792843a4bb05140a187c6c16776
AnandaRehan <anandarehan071@gmail.com>
2026-09-01 09:19:09 +0700

    update: 11

--- app/src/main/java/com/ehan/app2/MainActivity.kt
+++ app/src/main/java/com/ehan/app2/MainActivity.kt
@@ -32,7 +32,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.layout.systemBarsPadding
 // import androidx.compose.material3.*
-
 import androidx.compose.material3.Button
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -41,20 +40,15 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-
-import androidx.compose.runtime.*
-/**
+// import androidx.compose.runtime.*
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-*/
 import androidx.compose.runtime.saveable.rememberSaveable
-/**
 import androidx.compose.runtime.setValue
-*/
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
