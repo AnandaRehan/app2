9b2bc446d07a374fe7ce866d70b1c4eb56c24278
AnandaRehan <anandarehan071@gmail.com>
2026-09-02 07:51:15 +0700

    update: 15

--- app/src/main/java/com/ehan/app2/model/Models.kt
+++ app/src/main/java/com/ehan/app2/model/Models.kt
@@ -1,13 +1,18 @@
 package com.ehan.app2.model
 
-enum class PlayerPiece(val displayName: String, val shortName: String) {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+enum class PlayerPiece(val displayName: String, val shortName: String) : Parcelable {
     PLAYER_1("Pemain 1 (Merah)", "Merah"),
     PLAYER_2("Pemain 2 (Hitam)", "Hitam");
 
     fun opponent(): PlayerPiece = if (this == PLAYER_1) PLAYER_2 else PLAYER_1
 }
 
-data class Position(val row: Int, val col: Int) {
+@Parcelize
+data class Position(val row: Int, val col: Int) : Parcelable {
     fun isValid(): Boolean = row in 0..7 && col in 0..7
     fun isDarkSquare(): Boolean = (row + col) % 2 == 1
 
@@ -19,13 +24,15 @@ data class Position(val row: Int, val col: Int) {
         }
 }
 
+@Parcelize
 data class Piece(
     val player: PlayerPiece,
     val position: Position
-)
+): Parcelable
 
+@Parcelize
 data class Move(
     val from: Position,
     val to: Position,
     val player: PlayerPiece
-)
+) : Parcelable
